12732142f6eb4e469c1f949d6096ba517d79bb96
maimaliha21 <maimaliha8@gmail.com>
2024-03-09 14:58:36 +0200

    correvted the error

--- src/main/java/com/project1/project/Post/Post.java
+++ src/main/java/com/project1/project/Post/Post.java
@@ -1,49 +1,42 @@
 package com.project1.project.Post;
-
 import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.project1.project.Comment.Comment;
 import com.project1.project.Like.Like;
 import com.project1.project.Share.Share;
 import com.project1.project.User.User;
-
 import jakarta.persistence.*;
-
-
 @Entity
-
 @Table(name = "posts")
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long postId;
-
 // private Type type;
 private String image;
 private String video;
 private String content;
-private Long count =(long) 1;
+// private Long count =(long) 1;
+private static Long count = 0L; // متغير ثابت لزيادة العداد
+
   @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
-
     @OneToMany(mappedBy = "post")
     private List<Comment> comments;
 
-    @OneToMany(mappedBy = "post" ) //t8ayarat
-    public  List<Like> like;
+    @OneToMany(mappedBy = "post") //t8ayarat
+    private List<Like> like;
 
     @OneToMany(mappedBy = "post")
     private List<Share> shares;
-
-
     public Post() {
     }
 
-    
+ 
     public Post( String image, String video, String content, User user) {
+
         this.postId =++count;
         this.image = image;
         this.video = video;
@@ -57,20 +50,13 @@ private Long count =(long) 1;
         this.postId =++count;
         this.content = content;
         this.user = user;
-    
+
     }
-    // public void addLike(Like likeee) {
-    //     if(likeee==null)
-    //     new Like();
-    //     else{
-    //     like.add(likeee);
-    //     likeee.setPost(this);}
-    // }
+
 
     public Long getPostId() {
         return postId;
     }
-
     public void setPostId(Long postId) {
         this.postId = postId;
     }
@@ -78,50 +64,27 @@ private Long count =(long) 1;
         return user;
     }
 
-    public  void setUser(User user) {
+    public void setUser(User user) {
         this.user = user;
     }
-
 
     public String getImage() {
         return image;
     }
-
-
     public void setImage(String image) {
         this.image = image;
     }
-
-
     public String getVideo() {
         return video;
     }
-
-
     public void setVideo(String video) {
         this.video = video;
     }
-
-
     public String getContent() {
         return content;
     }
-
-
     public void setContent(String content) {
         this.content = content;
     }
-
-
-
-
-
-
-
-
-
-
     
-
 }
-
--- src/main/java/com/project1/project/Post/PostController.java
+++ src/main/java/com/project1/project/Post/PostController.java
@@ -1,28 +1,22 @@
 package com.project1.project.Post;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.project1.project.NFException;
 import com.project1.project.Comment.Comment;
 import com.project1.project.Like.Like;
 import com.project1.project.Like.likeType;
 import com.project1.project.User.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-
 
 @RestController
 @RequestMapping("/post") 
 public class PostController {
 
-    @Autowired
+@Autowired
     private  PostRepo postRepo;
 
 
@@ -30,7 +24,7 @@ public class PostController {
     // public Post createPost(Post post,User user){
     // //    post.setUser(user);
     // //    return postRepo.save(post);
-        
+
     // } Lazen nrj3 lma n3ml log in
 
 
@@ -40,22 +34,19 @@ public class PostController {
     }
 
 
+    public Post sharePost(Long postId,User user){
+        return null;
 
-    // public Post sharePost(Long postId,User user){
-    //     return null;
-        
-    // }
-    @GetMapping("/posts/{postId}")
+    }
+     @GetMapping("/posts/{postId}")
     public Post findById(@PathVariable Long postId){
         return postRepo.findById(postId).get();
         // .orElseThrow(new NFException(Post.class));
 
     }
-
     public void deleteById(Long postId ,Long userId){
 
     }
-
  public List<Post> getUserPost(User user){
     return null;
     
@@ -72,11 +63,12 @@ public class PostController {
  ////////////////////////////////////////////////////////////////////
  public List<Post> findByLikesContainsUser(User user){//البوستات الي اليوزر حاط لايك عليهم 
     return null;
-
  }  
-
-
  public Like likePost(Long postId,User user){//يوزر يوزر مش حاسها زابطة 
+    return null;
+
+}
+public List<Like> getAllPostLike(Long PostId){
     return null;
 
 }
@@ -87,10 +79,9 @@ public class PostController {
 //    return postRepo.findByLike(p);
 // }
 
-
 public Post postLike (Long postId,likeType type){//اذا في لايك بشيله اذا فش بحط
     return null;
-    
+
 }
 public Comment commentLike (Long postId,Long commentId,likeType type){//اذا في لايك بشيله اذا فش بحط
     return null;
@@ -101,14 +92,10 @@ public List<Like> getAllCommentLike(Long PostId){
     
 }
 //////////////////////////////////////////////////////
-
-
 public Post creatShare(Long postId,Long userId,String content){// بصير كانه بوست بوخذ اي دي جديد ;الكونتينت بكون ب البادي 
     return null;
     
 }
 public void deleteShearById(Long shearId ,Long userId){
-
 }
-
 }
