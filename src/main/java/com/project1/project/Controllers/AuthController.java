621d74c40ca1bbdff0fc9ab64d493fb5658f6ebb
maimaliha21 <maimaliha8@gmail.com>
2024-03-22 17:22:12 +0200

    fix some errors

--- src/main/java/com/project1/project/Controllers/AuthController.java
+++ src/main/java/com/project1/project/Controllers/AuthController.java
@@ -29,7 +29,7 @@ import com.project1.project.Payload.Response.MessageResponse;
 import com.project1.project.Security.Jwt.JwtUtils;
 import com.project1.project.Security.Services.UserDetailsImpl;
 
-
+//maii
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
