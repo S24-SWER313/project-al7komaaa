0a86c9feec16b76b493b65c5b85e33cddd95c20a
maimaliha21 <maimaliha8@gmail.com>
2024-04-06 00:11:31 +0300

    made entity Notification

---
+++ src/main/java/com/project1/project/Entity/Notification/Notification.java
@@ -0,0 +1,67 @@
+package com.project1.project.Entity.Notification;
+
+import java.time.LocalDateTime;
+import com.project1.project.Entity.User.User;
+import jakarta.persistence.*;
+
+@Entity
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @ManyToOne
+    private User recipient;
+    
+    private String content;
+    
+    private LocalDateTime timestamp;
+    
+    private boolean read;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(User recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public boolean isRead() {
+        return read;
+    }
+
+    public void setRead(boolean read) {
+        this.read = read;
+    }
+
+    public Notification() {
+    }
+
+ 
+    
+}
