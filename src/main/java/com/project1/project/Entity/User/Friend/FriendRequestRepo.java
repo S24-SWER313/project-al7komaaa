b5e0678813bce13d3dc7b11fdd54e0edc1569a3d
maimaliha21 <maimaliha8@gmail.com>
2024-05-24 17:22:10 +0300

    fri

--- src/main/java/com/project1/project/Entity/User/Friend/FriendRequestRepo.java
+++ src/main/java/com/project1/project/Entity/User/Friend/FriendRequestRepo.java
@@ -19,5 +19,12 @@ public interface FriendRequestRepo extends JpaRepository<FriendRequest, Long> {
     //void deleteBySenderAndReceiver(User sender, User receiver);
     //@Query("DELETE FROM friend_request fr WHERE fr.sender_id = :sender AND fr.receiver_id = :receiver")
     //void deleteBySenderAndReceiver(@Param("sender") Long sender_id, @Param("receiver") Long receiver_id);
+  
+      
+        // Optional<FriendRequest> findBySenderAndReceiverOrReceiverAndSender(User sender1, User receiver1, User sender2, User receiver2);
+        Optional<FriendRequest> findBySenderAndReceiverOrReceiverAndSender(User sender1, User receiver1, User sender2, User receiver2);
+        void deleteBySenderAndReceiverOrReceiverAndSender(User sender1, User receiver1, User sender2, User receiver2);
     
-}
+    }
+
+
