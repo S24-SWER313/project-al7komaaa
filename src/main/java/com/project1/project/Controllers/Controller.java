0cf72b9fce87e21704102790fcb039e88c9d26a5
Celina <202109356@bethlehem.edu>
2024-05-26 21:35:43 +0300

    mode

--- src/main/java/com/project1/project/Controllers/Controller.java
+++ src/main/java/com/project1/project/Controllers/Controller.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -697,8 +698,16 @@ public ResponseEntity<?> getBackgroundImage(@PathVariable Long userId) {
 }
 
 
-
-
+ @PutMapping("/editMode")
+    public ResponseEntity<Boolean> editMode() {
+        User user = userFromToken(request);
+        if (user == null) {
+            return ResponseEntity.ok(false);
+        }
+        user.setDark(!user.isDark());
+        userRepo.save(user);
+        return ResponseEntity.ok(user.isDark());
+    }
 
 
 
